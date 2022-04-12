b963fa9461d89480c9a9dacf183a308fc18bd8cc
sz <stefan.zischler@gmx.ch>
2022-04-12 15:11:17 +0200

    corrected Task 1 and probably finished Task 4

--- os-exercise-2-in-progress/os-exercise-2/code/T1.c
+++ os-exercise-2-in-progress/os-exercise-2/code/T1.c
@@ -21,73 +21,82 @@ int main ()
  	int arr[SIZE];         
 	for(int i=SIZE-1;i>=0;i--)
 		if(i%2==0)
-                	arr[(SIZE-1)-i]=0;
+            arr[(SIZE-1)-i]=0;
 		else
 			arr[(SIZE-1)-i]=1;
 
-        // TODO fork another process here
-		// One process should calculate the median
-		// The other process should calculate the mean
-		// One process should communicate its value to the other process
-		// The other process should print both calculated results
-		// Hint use shm_open, ftrucate, mmap, and mumap to communicate over mapped files
-		
-		//name of the shared memory object
-		//const char *name = "/T1";
-		//shared memory file decriptor
-		int pid = fork();
-		if (pid == 0) {
-			//child proccess
-			//create shared memory file
-			int fd;
-			fd = shm_open(NAME, O_CREAT | O_RDWR, 0777);
-			printf("fd: %d\n",fd);
-			if (fd < 0) {
-			//error message if file wasnt created
-			printf("Error creating shared memory: %s\n",strerror(errno));
-			exit(0);	
-			} else {
-				ftruncate(fd, 4096);
-				int *mean = 0;
-				int sum = 0;
-				//calculate mean over array
-				for (int i = 0; i < SIZE; i++) {
-					sum += arr[i];
-				}
-				mean = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_SHARED,fd,0);
-				*mean = sum / SIZE;
-				munmap (mean, 4096);
-				close(fd);
-			}
+	// TODO fork another process here
+	// One process should calculate the median
+	// The other process should calculate the mean
+	// One process should communicate its value to the other process
+	// The other process should print both calculated results
+	// Hint use shm_open, ftrucate, mmap, and mumap to communicate over mapped files
+	
+	//name of the shared memory object
+	//const char *name = "/T1";
+	//test array
+	/*
+	for (int i = 0; i < 10; i++) {
+		printf("Array at %d: %d\n", i, arr[i]);
+	}*/
+	//shared memory file decriptor
+	int pid = fork();
+	if (pid == 0) {
+		//child proccess
+		//print process ID
+		printf("Child process ID: %d\n", getpid());
+		//create shared memory file
+		int fd;
+		fd = shm_open(NAME, O_CREAT | O_RDWR, 0777);
+		printf("fd: %d\n",fd);
+		if (fd < 0) {
+		//error message if file wasnt created
+		printf("Error creating shared memory: %s\n",strerror(errno));
+		exit(0);	
 		} else {
-			//parent proccess
-			//calculate median
-			sort(arr, SIZE);
-			int median = 0;
-			if (SIZE % 2 == 0){
-				median = (arr[(SIZE-1) / 2] + arr[SIZE / 2]) / 2.0;
-			} else {
-				median = arr[SIZE/2];
+			ftruncate(fd, 4096);
+			float *mean = 0;
+			double sum = 0;
+			//calculate mean over array
+			for (int i = 0; i < SIZE; i++) {
+				sum += arr[i];
 			}
-			//wait for child to complete
-			wait(NULL);
-			//open shared memory
-			int fd = shm_open(NAME, O_CREAT|O_RDWR, 777);
-			if (fd < 0) {
-			//error message if file wasnt created
-			printf("Error creating shared memory: %s\n",strerror(errno));
-			exit(0);	
-			} else {
-				int *mean = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_SHARED,fd,0);
-				printf("Mean of array '%d': \n",*mean);
-				printf("Median of array '%d': \n",median);
-				munmap (0, 4096);
-				//close shared file
-				close(fd);
-				//remove link
-				shm_unlink(NAME);
-			}
+			mean = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_SHARED,fd,0);
+			*mean = (float)(sum / SIZE);
+			munmap (mean, 4096);
+			close(fd);
 		}
+	} else {
+		//parent proccess
+		//print proccess ID
+		printf("Parent process ID: %d\n", getpid());
+		//calculate median
+		sort(arr, SIZE);
+		double median = 0;
+		if (SIZE % 2 == 0){
+			median = (arr[(SIZE-1) / 2] + arr[SIZE / 2]) / 2.0;
+		} else {
+			median = arr[SIZE/2];
+		}
+		//wait for child to complete
+		wait(NULL);
+		//open shared memory
+		int fd = shm_open(NAME, O_CREAT|O_RDWR, 777);
+		if (fd < 0) {
+		//error message if file wasnt created
+		printf("Error creating shared memory: %s\n",strerror(errno));
+		exit(0);	
+		} else {
+			float *mean = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_SHARED,fd,0);
+			printf("Mean of array: '%f' \n",*mean);
+			printf("Median of array: '%f' \n",median);
+			munmap (0, 4096);
+			//close shared file
+			close(fd);
+			//remove link
+			shm_unlink(NAME);
+		}
+	}
 	return 0;
 }
 
--- os-exercise-2-in-progress/os-exercise-2/code/T4.c
+++ os-exercise-2-in-progress/os-exercise-2/code/T4.c
@@ -14,6 +14,8 @@ void init_processes(struct process *);
 void display(struct process *);
 void display_average_waiting_time(struct process *);
 void schedule_FCFS(struct process *);
+void schedule_SJF(struct process *);
+void schedule_LJF(struct process *);
 
 int main()
 {
@@ -23,11 +25,24 @@ int main()
 	printf("Orignial input (processes)\n");
 	display(processes);
 	
+	
 	schedule_FCFS(processes);
 	printf("Execution according to FCFS policy\n");
 	display(processes);
-	display_average_waiting_time(processes);	
+	display_average_waiting_time(processes);
+	
 
+	schedule_SJF(processes);
+	printf("Execution according to SJF policy\n");
+	display(processes);
+	display_average_waiting_time(processes);
+
+	
+	schedule_LJF(processes);
+	printf("Execution according to LJF policy\n");
+	display(processes);
+	display_average_waiting_time(processes);	
+	
 }
 void init_processes(struct process * input)
 {
@@ -53,20 +68,80 @@ void display(struct process * input){
 void schedule_FCFS(struct process * arr)
 {
 	//TODO
-	printf("Not implemented Yet!\n");
+	//initialize a process variable to hold processes
+	struct process *process = (struct process *) arr;
+	//initialize a temp variable 
+	struct process temp;
+	//switch the processes so they are in FCFS order
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = i + 1; j < SIZE; j++) {
+			if (process[i].arrival_time > process[j].arrival_time) {
+				temp = process[i];
+				process[i] = process[j];
+				process[j] = temp; 
+			}
+		}
+	}
+	// printf("Not implemented Yet!\n");
 }
 void schedule_SJF(struct process * arr)
 {
-        //TODO
-        printf("Not implemented Yet!\n");
+	//TODO
+	//initialize a process variable to hold processes
+	struct process *process = (struct process *) arr;
+	//initialize a temp variable 
+	struct process temp;
+	//switch the processes so they are in SJF order
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = i + 1; j < SIZE; j++) {
+			if (process[i].execution_time > process[j].execution_time) {
+				temp = process[i];
+				process[i] = process[j];
+				process[j] = temp; 
+			}
+		}
+	}
+	//printf("Not implemented Yet!\n");
 }
 void schedule_LJF(struct process * arr)
 {
-        //TODO
-        printf("Not implemented Yet!\n");
+	//TODO
+	//initialize a process variable to hold processes
+	struct process *process = (struct process *) arr;
+	//initialize a temp variable 
+	struct process temp;
+	//switch the processes so they are in LJF order
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = i + 1; j < SIZE; j++) {
+			if (process[i].execution_time < process[j].execution_time) {
+				temp = process[i];
+				process[i] = process[j];
+				process[j] = temp; 
+			}
+		}
+	}
+	//printf("Not implemented Yet!\n");
 }
 void display_average_waiting_time(struct process * arr)
 {
 	//TODO
-	printf("Not implemented Yet!\n");
+	//initialize a process variable to hold processes
+	struct process *process = (struct process *) arr;
+	//create waiting_time array with the waiting times for each process
+	int waiting_time[SIZE];
+	waiting_time[0]  = 0;
+	//calculate the waiting time for each process
+	for (int i = 1; i < SIZE; i++) {
+		waiting_time[i] = process[i-1].execution_time - waiting_time[i-1];
+		printf("Waiting time: %d\n", waiting_time[i]);
+	}
+	//calculate total waiting time of all processes
+	int total_wait_time = 0;
+	for (int i = 0; i < SIZE; i++) {
+		total_wait_time += waiting_time[i];
+	}
+	//calculate average waiting time over all processes
+	int avrg_time =(float)total_wait_time / (float)SIZE;
+	//display average waiting time
+	printf("Average waiting time: %d\n", avrg_time);
 }
