b9863278b6d7f3a26feb3012c443f98506a6d2c6
sebastianLukas1 <sebastian.lukas@stud.unibas.ch>
2022-05-04 15:58:36 +0200

    solved 1-2

--- os-exercise-4/code/T1-2.c
+++ os-exercise-4/code/T1-2.c
@@ -16,7 +16,9 @@ void update(int arr1[],int arr2[])
 {
 	//TODO find and fix the issue here
 	for(int i=0;i<SIZE;i++)
-		arr2[arr1[i]]=1;
+		//%SIZE to force arr1[i] to be a valid (but still random)
+		//index of arr2
+		arr2[arr1[i]%SIZE]=1;
 } 
 void display(int arr[])
 {
